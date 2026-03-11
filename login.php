02e9e1baec6b3918d17db172c9c7777ad076cdfa
Lara Wada Oguido <lara.oguido@aluno.senai.br>
2026-03-11 09:57:30 -0300

    primeiro commit

---
+++ login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Início</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    <div class="login-card p-4 shadow-lg">
+        <div class="text-center mb-4">
+            <i class="bi bi-lightning-charge-fill" ></i>
+            <h3 class="mt-2">Dicionário escolar</h3>
+            <p class="text-muted small">Aprender mais, para formar bem!</p>
+        </div>
+  <form>
+   
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Senha</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    
+</form>
+   
+    </style>
+       <div class="mb-3 select-box">
+<select class="form-select">
+<option selected>Selecione uma opção</option>
+<option value="1">Sou aluno</option>
+<option value="2">Sou professor</option>
+</select>
+</div>
+<button type="button-1" class="btn btn-outline-info">Entrar</button>
+<script src="assets/js/login.js">
+ 
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</html>
